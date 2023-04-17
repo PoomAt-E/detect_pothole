9bc163043aa5361fdd6a4f2bc296d215e0e2cb4a
WooJoongKim <rlandwnd555@naver.com>
2023-04-17 23:03:23 +0900

    feat : 스토리지 관련 URL 오브젝트 추가

--- src/main/kotlin/com/detect_pothole/detect_pothole/infra/gcp/CommonCodes.kt
+++ src/main/kotlin/com/detect_pothole/detect_pothole/infra/gcp/CommonCodes.kt
@@ -1,5 +1,7 @@
 package com.detect_pothole.detect_pothole.infra.gcp
 
 object Codes{
-
+    const val STORAGE_BASE_URL = "https://storage.googleapis.com/diviction/user-profile/"
+    const val VIDEO_PATH = STORAGE_BASE_URL + "video/"
+    const val IMAGE_PATH = STORAGE_BASE_URL + "image/"
 }
