20bb1b8a23bfb21f48097b84eeb08ff422e177bd
Identityless <rlandwnd555@naver.com>
2023-04-26 16:49:00 +0900

    fix : Point객체가 아닌 x, y 값으로 받도록 수정

--- src/main/kotlin/com/detect_pothole/detect_pothole/domain/pothole/dto/PotholeRegistrationRequest.kt
+++ src/main/kotlin/com/detect_pothole/detect_pothole/domain/pothole/dto/PotholeRegistrationRequest.kt
@@ -1,12 +1,11 @@
 package com.detect_pothole.detect_pothole.domain.pothole.dto
 
-import org.locationtech.jts.geom.Point
-import java.math.BigDecimal
 
 data class PotholeRegistrationRequest(
         val geotabId: Long,
         val xacc: Double,
         val yacc: Double,
         val zacc: Double,
-        val point: Point
+        val x: Double,
+        val y: Double
 )
