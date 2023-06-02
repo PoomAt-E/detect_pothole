d46f2a3ee149bbbfcb855dc9a2c6fab9c3a16238
Identityless <rlandwnd555@naver.com>
2023-06-02 18:05:50 +0900

    fix : responseDTO로의 파싱 에러 수정

--- src/main/kotlin/com/detect_pothole/detect_pothole/domain/pothole/dto/PotholeResponse.kt
+++ src/main/kotlin/com/detect_pothole/detect_pothole/domain/pothole/dto/PotholeResponse.kt
@@ -2,17 +2,17 @@ package com.detect_pothole.detect_pothole.domain.pothole.dto
 
 import com.detect_pothole.detect_pothole.domain.pothole.entity.Pothole
 import org.locationtech.jts.geom.Point
-import java.math.BigDecimal
 
 class PotholeResponse(
         val id: Long,
         val geotabId: Long,
-        val xacc: Double,
-        val yacc: Double,
-        val zacc: Double,
+//        val xacc: Double,
+//        val yacc: Double,
+//        val zacc: Double,
 //        val videoURL: String,
         val imageURL: String,
-        val point: Point,
+        val x: Double,
+        val y: Double,
         val state: String,
 ) {
     companion object {
@@ -20,12 +20,13 @@ class PotholeResponse(
             return PotholeResponse(
                     pothole.id!!,
                     pothole.geotabId!!.id!!,
-                    pothole.xacc!!.toDouble(),
-                    pothole.yacc!!.toDouble(),
-                    pothole.zacc!!.toDouble(),
+//                    pothole.xacc!!.toDouble(),
+//                    pothole.yacc!!.toDouble(),
+//                    pothole.zacc!!.toDouble(),
 //                    pothole.videoURL!!,
                     pothole.imageURL!!,
-                    pothole.point!!,
+                    pothole.point!!.x,
+                    pothole.point!!.y,
                     pothole.state!!
             )
         }
